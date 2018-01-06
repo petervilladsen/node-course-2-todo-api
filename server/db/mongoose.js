af3574cd6614def117633e482893ccb5db3b8582
Peter Villadsen <pvilladsen@gmail.com>
2018-01-06 22:13:35 +0100

    corrected db username and passord

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const dbUrl = process.env.PORT ? 'mongodb://<dbuser>:<dbpassword>@ds247047.mlab.com:47047/todoap' : 'mongodb://localhost:27017/TodoApp';
+const dbUrl = process.env.PORT ? 'mongodb://todo:todo_@ds247047.mlab.com:47047/todoap' : 'mongodb://localhost:27017/TodoApp';
 // If db is on Heroku use process.env.MONGODB_URI
 
 mongoose.Promise = global.Promise;
